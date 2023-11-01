51f5f8cf375399adb5d28dcb27039628fc6e0384
Prashanth-KC <65711221+Prashanth-KC@users.noreply.github.com>
2023-11-01 11:14:01 -0700

    created and connected  to MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 // Load env variables
-require("dotenv").config();
-
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
 // Import dependencies
 const express = require("express");
-
+const connectToDb = require("./config/connectToDb");
 
 // Create an express app
 const app = express();
 
+//connect to database
+connectToDb();
+
 // Routing
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({ hello: "world" });
 });
 
